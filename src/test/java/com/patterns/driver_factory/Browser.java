2481e675aa55b05aeaec0746e2c72ddef5c7d9ca
MartyMcAir <oth-corp@yandex.ru>
2021-09-17 19:11:11 +0300

    is deleted for experimental

--- src/test/java/com/patterns/driver_factory/Browser.java
+++
@@ -1,7 +0,0 @@
-package com.patterns.driver_factory;
-
-import org.openqa.selenium.WebDriver;
-
-public enum Browser {
-    CHROME, IE, EDGE, FIREFOX, OPERA, SAFARI; // SAFARI - does not supported for Windows
-}
